0c48f68b1c6256b2f729f816eb76dc8ff3fd1f48
Zen1ne <zenaku1ne@gmail.com>
2024-06-25 15:02:04 -0600

    Creacion de conexion a la db

---
+++ Conexion.php
@@ -0,0 +1,12 @@
+<?php
+class Conexion{
+    private $host = "localhost";
+    private $user = "brandon";
+    private $password = "123";
+    private $bdname="db_sistema";
+    private $conect;
+    
+
+}
+
+?>
